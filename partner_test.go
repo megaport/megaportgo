87e20674bf0b9abc41e6242c56bc76c539c05f6a
Phil Browne <philip.browne@megaport.com>
2024-02-13 16:41:19 +0000

    chore: client unit tests for partner ports

---
+++ partner_test.go
@@ -0,0 +1,322 @@
+package megaport
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+)
+
+var (
+	companyUid = "32df7107-fdca-4c2a-8ccb-c6867813b3f2"
+	companyName = "Test Company"
+	companyUid2 = "2111de9a-6211-40e3-8e6a-65ab106d01f4"
+	companyName2 = "Other Company"
+	productUid = "36b3f68e-2f54-4331-bf94-f8984449365f"
+	productUid2 = "9b1c46c7-1e8d-4035-bf38-1bc60d346d57"
+	productUid3 = "91ededc2-473f-4a30-ad24-0703c7f35e50"
+	productUid4 = "9a05f787-8166-4470-94f1-4906db86f698"
+	partnerMegaportUrl = "/v2/dropdowns/partner/megaports"
+)
+
+type PartnerClientTestSuite struct {
+	ClientTestSuite
+}
+
+func TestPartnerClientTestSuite(t *testing.T) {
+	suite.Run(t, new(PartnerClientTestSuite))
+}
+
+func (suite *PartnerClientTestSuite) SetupTest() {
+	suite.mux = http.NewServeMux()
+	suite.server = httptest.NewServer(suite.mux)
+
+	suite.client = NewClient(nil, nil)
+	url, _ := url.Parse(suite.server.URL)
+	suite.client.BaseURL = url
+}
+
+func (suite *PartnerClientTestSuite) TearDownTest() {
+	suite.server.Close()
+}
+
+var awsPartner = &PartnerMegaport{
+	ProductUID: productUid,
+	CompanyUID: companyUid,
+	CompanyName: companyName,
+	ProductName: "Test Partner AWS",
+	LocationId: 1,
+	Speed: 10000,
+	DiversityZone: "red",
+	ConnectType: "AWS",
+	VXCPermitted: true,
+}
+
+var azurePartner = &PartnerMegaport{
+	ProductUID: productUid2,
+	CompanyUID: companyUid,
+	CompanyName: companyName,
+	ProductName: "Test Partner Azure",
+	LocationId: 2,
+	Speed: 10000,
+	ConnectType: "AZURE",
+	VXCPermitted: true,
+}
+
+var defaultPartner = &PartnerMegaport{
+	ProductUID: productUid3,
+	CompanyUID: companyUid2,
+	CompanyName: companyName2,
+	ProductName: "Partner Default",
+	LocationId: 3,
+	Speed: 10000,
+	DiversityZone: "red",
+	ConnectType: "DEFAULT",
+	VXCPermitted: true,
+}
+
+var awsHcPartner = &PartnerMegaport{
+	ProductUID: productUid4,
+	CompanyUID: companyUid2,
+	CompanyName: companyName2,
+	ProductName: "Partner AWSHC",
+	LocationId: 3,
+	Speed: 10000,
+	ConnectType: "AWSHC",
+	VXCPermitted: true,
+}
+
+var jblob = `{
+	"message": "All Partner Megaports",
+	"terms": "This data is subject to the Acceptable Use Policy https://www.megaport.com/legal/acceptable-use-policy",
+	"data": [
+		{
+			"productUid": "36b3f68e-2f54-4331-bf94-f8984449365f",
+			"companyUid": "32df7107-fdca-4c2a-8ccb-c6867813b3f2",
+			"companyName": "Test Company",
+			"title": "Test Partner AWS",
+			"locationId": 1,
+			"speed": 10000,
+			"maxVxcSpeed": 10000,
+			"vxcPermitted": true,
+			"diversityZone": "red",
+			"connectType": "AWS"
+        },
+        {
+			"productUid": "9b1c46c7-1e8d-4035-bf38-1bc60d346d57",
+			"companyUid": "32df7107-fdca-4c2a-8ccb-c6867813b3f2",
+			"companyName": "Test Company",
+			"title": "Test Partner Azure",
+			"locationId": 2,
+			"speed": 10000,
+			"maxVxcSpeed": 10000,
+			"vxcPermitted": true,
+			"diversityZone": null,
+			"connectType": "AZURE"
+        },
+        {
+			"productUid": "91ededc2-473f-4a30-ad24-0703c7f35e50",
+			"companyUid": "2111de9a-6211-40e3-8e6a-65ab106d01f4",
+			"companyName": "Other Company",
+			"title": "Partner Default",
+			"locationId": 3,
+			"speed": 10000,
+			"maxVxcSpeed": 10000,
+			"vxcPermitted": true,
+			"diversityZone": "red",
+			"connectType": "DEFAULT"
+        },
+        {
+			"productUid": "9a05f787-8166-4470-94f1-4906db86f698",
+			"companyUid": "2111de9a-6211-40e3-8e6a-65ab106d01f4",
+			"companyName": "Other Company",
+			"title": "Partner AWSHC",
+			"locationId": 3,
+			"speed": 10000,
+			"maxVxcSpeed": 10000,
+			"vxcPermitted": true,
+			"diversityZone": null,
+			"connectType": "AWSHC"
+        }
+    ]
+}`
+
+func (suite *PartnerClientTestSuite) TestListPartnerMegaports() {
+	partnerSvc := suite.client.PartnerService
+	ctx := context.Background()
+
+	want := []*PartnerMegaport{awsPartner, azurePartner, defaultPartner, awsHcPartner}
+
+	suite.mux.HandleFunc(partnerMegaportUrl, func(w http.ResponseWriter, r *http.Request) {
+		suite.testMethod(r, http.MethodGet)
+		fmt.Fprint(w, jblob)
+	})
+
+	partners, err := partnerSvc.ListPartnerMegaports(ctx)
+	suite.NoError(err)
+	suite.Equal(want, partners)
+}
+
+// 	FilterPartnerMegaportByLocationId(ctx context.Context, partners []*PartnerMegaport, locationId int) ([]*PartnerMegaport, error)
+// 	FilterPartnerMegaportByDiversityZone(ctx context.Context, partners []*PartnerMegaport, diversityZone string, exactMatch bool) ([]*PartnerMegaport, error)
+
+
+
+func (suite *PartnerClientTestSuite) TestFilterPartnerMegaportByProductName() {
+	partnerSvc := suite.client.PartnerService
+	ctx := context.Background()
+
+	want := []*PartnerMegaport{awsPartner, azurePartner, defaultPartner, awsHcPartner}
+
+	suite.mux.HandleFunc(partnerMegaportUrl, func(w http.ResponseWriter, r *http.Request) {
+		suite.testMethod(r, http.MethodGet)
+		fmt.Fprint(w, jblob)
+	})
+
+	partners, err := partnerSvc.ListPartnerMegaports(ctx)
+	suite.NoError(err)
+	suite.Equal(want, partners)
+
+	wantFiltered := []*PartnerMegaport{awsPartner, azurePartner}
+
+	gotFiltered, err := partnerSvc.FilterPartnerMegaportByProductName(ctx, partners, "Test", false)
+	suite.NoError(err)
+	suite.Equal(wantFiltered, gotFiltered)
+}
+
+func (suite *PartnerClientTestSuite) TestFilterPartnerMegaportByProductNameExact() {
+	partnerSvc := suite.client.PartnerService
+	ctx := context.Background()
+
+	want := []*PartnerMegaport{awsPartner, azurePartner, defaultPartner, awsHcPartner}
+
+	suite.mux.HandleFunc(partnerMegaportUrl, func(w http.ResponseWriter, r *http.Request) {
+		suite.testMethod(r, http.MethodGet)
+		fmt.Fprint(w, jblob)
+	})
+
+	partners, err := partnerSvc.ListPartnerMegaports(ctx)
+	suite.NoError(err)
+	suite.Equal(want, partners)
+
+	wantFiltered := []*PartnerMegaport{awsPartner}
+
+	gotFiltered, err := partnerSvc.FilterPartnerMegaportByProductName(ctx, partners, "Test Partner AWS", true)
+	suite.NoError(err)
+	suite.Equal(wantFiltered, gotFiltered)
+}
+
+func (suite *PartnerClientTestSuite) TestFilterPartnerMegaportByConnectType() {
+	partnerSvc := suite.client.PartnerService
+	ctx := context.Background()
+
+	want := []*PartnerMegaport{awsPartner, azurePartner, defaultPartner, awsHcPartner}
+
+	suite.mux.HandleFunc(partnerMegaportUrl, func(w http.ResponseWriter, r *http.Request) {
+		suite.testMethod(r, http.MethodGet)
+		fmt.Fprint(w, jblob)
+	})
+
+	partners, err := partnerSvc.ListPartnerMegaports(ctx)
+	suite.NoError(err)
+	suite.Equal(want, partners)
+
+	wantFiltered := []*PartnerMegaport{awsPartner, awsHcPartner}
+
+	gotFiltered, err := partnerSvc.FilterPartnerMegaportByConnectType(ctx, partners, "AWS", false)
+	suite.NoError(err)
+	suite.Equal(wantFiltered, gotFiltered)
+}
+
+func (suite *PartnerClientTestSuite) TestFilterPartnerMegaportByConnectTypeExact() {
+	partnerSvc := suite.client.PartnerService
+	ctx := context.Background()
+
+	want := []*PartnerMegaport{awsPartner, azurePartner, defaultPartner, awsHcPartner}
+
+	suite.mux.HandleFunc(partnerMegaportUrl, func(w http.ResponseWriter, r *http.Request) {
+		suite.testMethod(r, http.MethodGet)
+		fmt.Fprint(w, jblob)
+	})
+
+	partners, err := partnerSvc.ListPartnerMegaports(ctx)
+	suite.NoError(err)
+	suite.Equal(want, partners)
+
+	wantFiltered := []*PartnerMegaport{awsHcPartner}
+
+	gotFiltered, err := partnerSvc.FilterPartnerMegaportByConnectType(ctx, partners, "AWSHC", true)
+	suite.NoError(err)
+	suite.Equal(wantFiltered, gotFiltered)
+}
+
+
+func (suite *PartnerClientTestSuite) TestFilterPartnerMegaportByCompanyName() {
+	partnerSvc := suite.client.PartnerService
+	ctx := context.Background()
+
+	want := []*PartnerMegaport{awsPartner, azurePartner, defaultPartner, awsHcPartner}
+
+	suite.mux.HandleFunc(partnerMegaportUrl, func(w http.ResponseWriter, r *http.Request) {
+		suite.testMethod(r, http.MethodGet)
+		fmt.Fprint(w, jblob)
+	})
+
+	partners, err := partnerSvc.ListPartnerMegaports(ctx)
+	suite.NoError(err)
+	suite.Equal(want, partners)
+
+	wantFiltered := []*PartnerMegaport{awsPartner, azurePartner}
+
+	gotFiltered, err := partnerSvc.FilterPartnerMegaportByCompanyName(ctx, partners, companyName, true)
+	suite.NoError(err)
+	suite.Equal(wantFiltered, gotFiltered)
+}
+
+func (suite *PartnerClientTestSuite) TestFilterPartnerMegaportByLocationId() {
+	partnerSvc := suite.client.PartnerService
+	ctx := context.Background()
+
+	want := []*PartnerMegaport{awsPartner, azurePartner, defaultPartner, awsHcPartner}
+
+	suite.mux.HandleFunc(partnerMegaportUrl, func(w http.ResponseWriter, r *http.Request) {
+		suite.testMethod(r, http.MethodGet)
+		fmt.Fprint(w, jblob)
+	})
+
+	partners, err := partnerSvc.ListPartnerMegaports(ctx)
+	suite.NoError(err)
+	suite.Equal(want, partners)
+
+	wantFiltered := []*PartnerMegaport{defaultPartner, awsHcPartner}
+
+	gotFiltered, err := partnerSvc.FilterPartnerMegaportByLocationId(ctx, partners, 3)
+	suite.NoError(err)
+	suite.Equal(wantFiltered, gotFiltered)
+}
+
+func (suite *PartnerClientTestSuite) TestFilterPartnerMegaportByDiversityZone() {
+	partnerSvc := suite.client.PartnerService
+	ctx := context.Background()
+
+	want := []*PartnerMegaport{awsPartner, azurePartner, defaultPartner, awsHcPartner}
+
+	suite.mux.HandleFunc(partnerMegaportUrl, func(w http.ResponseWriter, r *http.Request) {
+		suite.testMethod(r, http.MethodGet)
+		fmt.Fprint(w, jblob)
+	})
+
+	partners, err := partnerSvc.ListPartnerMegaports(ctx)
+	suite.NoError(err)
+	suite.Equal(want, partners)
+
+	wantFiltered := []*PartnerMegaport{awsPartner, defaultPartner}
+
+	gotFiltered, err := partnerSvc.FilterPartnerMegaportByDiversityZone(ctx, partners, "red", true)
+	suite.NoError(err)
+	suite.Equal(wantFiltered, gotFiltered)
+}
