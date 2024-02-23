629c67a2bec44c13adbdca338178cee487d184b7
Phil Browne <philip.browne@megaport.com>
2024-02-23 15:02:10 +0000

    docs: godocs for products service

--- product.go
+++ product.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 )
 
+// ProductService is an interface for interfacing with the Product endpoints of the Megaport API.
 type ProductService interface {
 	ExecuteOrder(ctx context.Context, requestBody interface{}) (*[]byte, error)
 	ModifyProduct(ctx context.Context, req *ModifyProductRequest) (*ModifyProductResponse, error)
@@ -26,6 +27,7 @@ func NewProductService(c *Client) *ProductServiceOp {
 	}
 }
 
+// ModifyProductRequest represents a request to modify a product in the Megaport Products API.
 type ModifyProductRequest struct {
 	ProductID             string
 	ProductType           string
@@ -34,35 +36,45 @@ type ModifyProductRequest struct {
 	MarketplaceVisibility bool   `json:"marketplaceVisibility,omitempty"`
 }
 
+// ModifyProductResponse represents a response from the Megaport Products API after modifying a product.
 type ModifyProductResponse struct {
 	IsUpdated bool
 }
+
+// DeleteProductRequest represents a request to delete a product in the Megaport Products API.
 type DeleteProductRequest struct {
 	ProductID string
 	DeleteNow bool
 }
 
+// DeleteProductResponse represents a response from the Megaport Products API after deleting a product.
 type DeleteProductResponse struct{}
 
+// RestoreProductRequest represents a request to restore a product in the Megaport Products API.
 type RestoreProductRequest struct {
 	ProductID string
 }
 
+// RestoreProductResponse represents a response from the Megaport Products API after restoring a product.
 type RestoreProductResponse struct{}
 
+// ManageProductLockRequest represents a request to lock or unlock a product in the Megaport Products API.
 type ManageProductLockRequest struct {
 	ProductID  string
 	ShouldLock bool
 }
 
+// ManageProductLockResponse represents a response from the Megaport Products API after locking or unlocking a product.
 type ManageProductLockResponse struct{}
 
+// ParsedProductsResponse represents a response from the Megaport Products API prior to parsing the response.
 type ParsedProductsResponse struct {
 	Message string        `json:"message"`
 	Terms   string        `json:"terms"`
 	Data    []interface{} `json:"data"`
 }
 
+// ExecuteOrder is responsible for executing an order for a product in the Megaport Products API.
 func (svc *ProductServiceOp) ExecuteOrder(ctx context.Context, requestBody interface{}) (*[]byte, error) {
 	path := "/v3/networkdesign/buy"
 
@@ -91,7 +103,7 @@ func (svc *ProductServiceOp) ExecuteOrder(ctx context.Context, requestBody inter
 	return &body, nil
 }
 
-// ModifyProduct modifies a product. The available fields to modify are Name, Cost Centre, and Marketplace Visibility.
+// ModifyProduct modifies a product in the Megaport Products API. The available fields to modify are Name, Cost Centre, and Marketplace Visibility.
 func (svc *ProductServiceOp) ModifyProduct(ctx context.Context, req *ModifyProductRequest) (*ModifyProductResponse, error) {
 	if req.ProductType == PRODUCT_MEGAPORT || req.ProductType == PRODUCT_MCR || req.ProductType == PRODUCT_MVE {
 		path := fmt.Sprintf("/v2/product/%s/%s", req.ProductType, req.ProductID)
@@ -113,8 +125,7 @@ func (svc *ProductServiceOp) ModifyProduct(ctx context.Context, req *ModifyProdu
 	}
 }
 
-// DeleteProduct is responsible for either scheduling a product for deletion "CANCEL" or deleting a product immediately
-// "CANCEL_NOW".
+// DeleteProduct is responsible for either scheduling a product for deletion "CANCEL" or deleting a product immediately "CANCEL_NOW" in the Megaport Products API.
 func (svc *ProductServiceOp) DeleteProduct(ctx context.Context, req *DeleteProductRequest) (*DeleteProductResponse, error) {
 	var action string
 
@@ -139,6 +150,7 @@ func (svc *ProductServiceOp) DeleteProduct(ctx context.Context, req *DeleteProdu
 	return &DeleteProductResponse{}, nil
 }
 
+// RestoreProduct is responsible for restoring a product in the Megaport Products API. The product must be in a "CANCELLED" state to be restored.
 func (svc *ProductServiceOp) RestoreProduct(ctx context.Context, productId string) (*RestoreProductResponse, error) {
 	path := "/v3/product/" + productId + "/action/UN_CANCEL"
 	url := svc.Client.BaseURL.JoinPath(path).String()
@@ -154,6 +166,7 @@ func (svc *ProductServiceOp) RestoreProduct(ctx context.Context, productId strin
 	return &RestoreProductResponse{}, nil
 }
 
+// ManageProductLock is responsible for locking or unlocking a product in the Megaport Products API.
 func (svc *ProductServiceOp) ManageProductLock(ctx context.Context, req *ManageProductLockRequest) (*ManageProductLockResponse, error) {
 	verb := "POST"
 
