cbd9344321264e4475a2341c4c805beb3a7c2978
Phil Browne <philip.browne@megaport.com>
2024-02-23 15:15:49 +0000

    docs: shared types

--- shared_types.go
+++ shared_types.go
@@ -9,25 +9,32 @@ const (
 	SERVICE_CONFIGURED = "CONFIGURED" // The CONFIGURED service state.
 	SERVICE_LIVE       = "LIVE"       // The LIVE service state.
 
+	// Product types
 	PRODUCT_MEGAPORT = "megaport"
 	PRODUCT_VXC      = "vxc"
 	PRODUCT_MCR      = "mcr2"
 	PRODUCT_MVE      = "mve"
 	PRODUCT_IX       = "ix"
 
+	// Cancellation states
 	STATUS_DECOMMISSIONED = "DECOMMISSIONED"
 	STATUS_CANCELLED      = "CANCELLED"
 
+	// Port Types
 	SINGLE_PORT                        = "Single"
 	LAG_PORT                           = "LAG"
+
+	// AWS VXC Types
 	CONNECT_TYPE_AWS_VIF               = "AWS"
 	CONNECT_TYPE_AWS_HOSTED_CONNECTION = "AWSHC"
 )
 
 var (
+	// SERVICE_STATE_READY is a list of service states that are considered ready for use.
 	SERVICE_STATE_READY = []string{SERVICE_CONFIGURED, SERVICE_LIVE}
 )
 
+// GenericResponse is a generic response structure for API responses.
 type GenericResponse struct {
 	Message string                 `json:"message"`
 	Terms   string                 `json:"terms"`
@@ -42,10 +49,12 @@ const MODIFY_RATE_LIMIT = "RATE_LIMIT"
 const MODIFY_A_END_VLAN = "A_VLAN"
 const MODIFY_B_END_VLAN = "B_VLAN"
 
+// Time is a custom time type that allows for unmarshalling of Unix timestamps.
 type Time struct {
  	time.Time
 }
 
+// UnmarshalJSON unmarshals a Unix timestamp into a Time type.
 func (t *Time) UnmarshalJSON(b []byte) error {
 	var timestamp int64
 	err := json.Unmarshal(b, &timestamp)
