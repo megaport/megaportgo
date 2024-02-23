6ed6d999d0b1a6885e5f40bd67208b5ad09aed94
Phil Browne <philip.browne@megaport.com>
2024-02-23 15:44:42 +0000

    docs: megaport test

--- megaport_test.go
+++ megaport_test.go
@@ -5,15 +5,19 @@ import (
 	"log/slog"
 )
 
+// runIntegrationTests is a flag to determine if integration tests should be run
 var runIntegrationTests = flag.Bool("integration", false, "perform integration tests")
 
 var accessKey string
 var secretKey string
 
+// megaportClient is the client for the test suite
 var megaportClient *Client
 
+// programLevel is the log level for the test suite
 var programLevel = new(slog.LevelVar)
 
+// Default Base URL for Integration Tests
 const (
 	MEGAPORTURL = "https://api-staging.megaport.com/"
 )
