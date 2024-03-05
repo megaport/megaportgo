429298ad7aa41034c8b70fe009e7ec8061e65265
Alex Godwin <alex.godwin@megaport.com>
2024-03-05 13:32:21 -0500

    docs: adds example for custom logger

--- example_test.go
+++ example_test.go
@@ -3,6 +3,8 @@ package megaport_test
 import (
 	"context"
 	"fmt"
+	"log/slog"
+	"os"
 
 	megaport "github.com/megaport/megaportgo"
 )
@@ -35,4 +37,25 @@ func Example() {
 	for _, location := range locations {
 		fmt.Println(location.Name)
 	}
+}
+
+// Example with a custom logger
+func Example_logger() {
+	// A handler that logs JSON logs to stdout but only errors
+	handler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelError,
+	})
+
+	// Create a new client with your custom log handler
+	client, err := megaport.New(nil,
+		megaport.WithCredentials("ACCESS_KEY", "SECRET_KEY"),
+		megaport.WithEnvironment(megaport.EnvironmentStaging),
+		megaport.WithLogHandler(handler),
+	)
+	if err != nil {
+		// ...
+	}
+
+	client.Logger.ErrorContext(context.Background(), "testing") // will print
+	client.Logger.InfoContext(context.Background(), "testing")  // won't print
 }
