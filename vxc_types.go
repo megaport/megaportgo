81b9b3fed26e9f9ad1ab2d4ace2e809f0dc2e68a
Phil Browne <philip.browne@megaport.com>
2024-04-30 20:49:50 +0000

    fix: change virtual router name to vrouter in other spot

--- vxc_types.go
+++ vxc_types.go
@@ -517,7 +517,7 @@ func (c *CSPConnection) UnmarshalJSON(data []byte) error {
 				return err
 			}
 			c.CSPConnection = append(c.CSPConnection, azure)
-		case "VIRTUAL_ROUTER":
+		case "VROUTER":
 			marshaled, err := json.Marshal(cn)
 			if err != nil {
 				return err
@@ -607,6 +607,7 @@ func (c *CSPConnection) UnmarshalJSON(data []byte) error {
 				if err := json.Unmarshal(marshaled, &vr); err != nil {
 					return err
 				}
+				fmt.Println("got yourself a virtual router!!!")
 				c.CSPConnection = append(c.CSPConnection, vr)
 			case "TRANSIT":
 				marshaled, err := json.Marshal(cn)
